4412d61e6b287beed64d2129167744861b3a162e
DrJamgo <drjamgo@hotmail.com>
2020-06-09 18:13:14 +0200

    FEAT: Final

---
+++ conf.lua
@@ -0,0 +1,6 @@
+-- from: https://love2d.org/wiki/Config_Files
+
+function love.conf(t)
+  t.window.width = 1280                -- The window width (number)
+  t.window.height = 640               -- The window height (number)
+end
